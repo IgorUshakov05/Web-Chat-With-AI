e0461948f86189b1cfcfeb589c97e11037c5b2f3
Igor <igorushakov111@gmail.com>
2025-05-07 11:52:09 +0700

    dev

--- app/src/Meta/Index.tsx
+++ app/src/Meta/Index.tsx
@@ -5,7 +5,7 @@ export default function IndexMeta() {
 
   return (
     <Helmet>
-      <title>HuntAI - Нейросеть для создания решений</title>
+      <title>HuntAI Игорь - Нейросеть для создания решений</title>
 
       {/* Description */}
       <meta
